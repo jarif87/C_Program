4db2555d682fd579c3291cbea4427b4461e3dd7c
jarif87 <78945097+jarif87@users.noreply.github.com>
2022-09-08 21:08:48 +0600

    first commit

---
+++ add two number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main () 
+{
+  
+int a, b;
+  
+ 
+printf ("Enter your first number %d\n");
+  
+scanf ("%d", &a);
+  
+printf ("Enter your second number %d\n");
+  
+scanf ("%d", &b);
+  
+printf ("Sum of a and b is %d", a + b);
+  
+ 
+ 
+ 
+ 
+return 0;
+
+}
+
+
+ 
---
+++ variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int
+main ()
+{
+  int a = 8;
+  float b = 9.5;
+  char c = "h";
+  int f = 47;
+
+  printf ("Value of a is %d\n", a);
+  printf ("Value of a is %f\n", b);
+  printf ("Value of a is %c\n", c);
+  printf ("sum of a and f is %d\n", a + f);
+  return 0;
+
+}

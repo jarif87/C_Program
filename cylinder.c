a18e24c2c9c98005cac8f7ff68f7300dc8fdece9
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-09-09 01:31:54 +0600

    Add files via upload

---
+++ cylinder.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int radious=3;
+    float pi=3.14;
+    printf("the area of circle is %f\n",pi*radious*radious);
+    int height=3;
+    printf("volume of this cylinder is %f\n",pi*height*height);
+    return 0;
+}

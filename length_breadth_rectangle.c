3762e533a8e523bf49ddc686fd1891f6082117df
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-09-09 01:59:08 +0600

    Add files via upload

---
+++ length_breadth_rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int length,breadth;
+    printf("What is the length of rectangle\n");
+    scanf("%d",&length);
+    printf("What is the breadth of rectangle\n");
+    scanf("%d",&breadth);
+    printf("the area of rectangle is %d",length*breadth);
+
+    return 0;
+}

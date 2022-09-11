b89ac48501cda25049bfd880ccd19844a23769ad
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-09-12 02:28:08 +0600

    Add files via upload

---
+++ calculate simple interest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int principle=1000,rate=4,years=1;
+    int simple_interest=(principle*rate*years);
+
+    printf("The value of simple interes is %d",simple_interest);
+
+
+    return 0;
+}

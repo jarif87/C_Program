f855dcabaacf85c30f10ccc854882957580a1fc0
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-09-12 03:16:01 +0600

    Add files via upload

---
+++ arithmetic instruction.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a=4,b=10;
+
+    int z=b*a;
+    printf("value of  z is %d\n",z);
+
+    printf("the value of a+b is %d\n",a+b);
+    printf("the value of a-b is %d\n",a-b);
+    printf("the value of a*b is %d\n",a*b);
+    printf("the value of a/b is %d\n",a/b);
+
+
+    printf("value of a%b is %d\n",8%3);
+    printf("value of 4 to the power 6 is %.f\n",pow(4,6));
+
+    return 0;
+}
---
+++ check value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a=1.2,b=9;
+    float c=10+a;
+
+    printf("Number of c is %.f\n",c);
+
+
+    return 0;
+}

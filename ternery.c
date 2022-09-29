46726fa0df131c7f74938618bc9f844cbcd3275f
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-09-30 02:41:35 +0600

    Add files via upload

---
+++ ternery.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("Enter your number:");
+  scanf("%d",&num);
+  (num<7) ? printf("num is less than seven") : printf("num is greater than seven");
+
+
+  return 0;
+}

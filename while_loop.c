2efc71ff576f4515888f5959c4f399d1cbd53ada
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-10-01 20:07:21 +0600

    Add files via upload

---
+++ while_loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int a;
+    printf("Enter your number:");
+    scanf("%d",&a);
+    while(a<10)
+    {
+
+        printf("shakib al hasan no1 allrounder %d\n",a);
+        a++;
+    }
+
+
+    return 0;
+}

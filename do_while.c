5a9f2f5c44074a703b2472c8e103dfc145bb1fc9
sadik al jarif <78945097+jarif87@users.noreply.github.com>
2022-10-01 20:29:22 +0600

    Add files via upload

---
+++ do_while.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=3;
+    do
+    {
+        printf("the value of a is %d\n",a);
+        a++;
+    }
+    while(a<20);
+
+
+    return 0;
+}
---
+++ increment.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i=5;
+    int a;
+
+    printf("value of ++i  is %d\n",++i);
+    printf("value of i++ is %d\n",i++);
+    printf("value of i  is %d\n",i);
+
+    a+=10;
+    printf("value of a is %d\n",a);
+    return 0;
+}
